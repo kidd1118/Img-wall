0f538305a25c0f944780d40ba142cb11ce5883bd
Leo.Lee <Leo.Lee@xuenn.com>
2018-02-05 15:52:00 +0800

    testing data

--- src/components/ImgWall.vue
+++ src/components/ImgWall.vue
@@ -144,6 +144,11 @@ export default {
         { thumbnailId: 8, name: "08" },
         { thumbnailId: 2, name: "02" },
         { thumbnailId: 3, name: "03" },
+        { thumbnailId: 4, name: "04" },
+        { thumbnailId: 8, name: "08" },
+        { thumbnailId: 6, name: "06" },
+        { thumbnailId: 4, name: "04" },
+        { thumbnailId: 5, name: "05" },
         { thumbnailId: 4, name: "04" }
       ];
 
